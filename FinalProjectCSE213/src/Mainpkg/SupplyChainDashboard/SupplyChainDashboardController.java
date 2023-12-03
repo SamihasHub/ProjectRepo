7b1576d544722c97c82dbeb3f71e5ad0a7fd8e8e
shamit09 <2110334@iub.edu.bd>
2023-12-03 21:19:53 +0600

    Supply Chain Scenes

---
+++ FinalProjectCSE213/src/Mainpkg/SupplyChainDashboard/SupplyChainDashboardController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package finalproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author shamit reza
+ */
+public class SupplyChainDashboardController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void OpenStocksOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void OpenGoodsOrderedOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void OpenNewOrdersOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void ViewOptionOnClick(ActionEvent event) {
+    }
+    
+}
